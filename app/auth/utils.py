e7b4828d5eeffb81dd92cc503c977e2ed5cbbb47
Moksha <nmoksha.17@gmail.com>
2026-04-26 23:35:44 +0530

    util.py changes

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -17,6 +17,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str) -> str:
     return pwd_context.hash(password[:72])
 
+print("PASSWORD RECEIVED:", payload.password)
+print("LENGTH:", len(payload.password))
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     if not hashed_password:
